3a5ca0ccb89c27ffb813116fdefa3ebaac3cfc34
Archie <azwerlein@my.wctc.edu>
2023-04-03 14:58:54 -0500

    Set up logger

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NLog;
+
+string path = Directory.GetCurrentDirectory() + "\\nlog.config;";
+
+var Logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
